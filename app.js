dd2ef48c22af0733e90b8391b5fa99cf9e911e02
nishantssr <nishantssr51@gmail.com>
2016-03-16 12:00:53 +0100

    Original Code
    Original Code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var app = angular.module('testapp', ['ngRoute',]);
-
-app.config(function($routeProvider){
+angular.module('testapp', [
+	'ngRoute',
+]).
+config(function($routeProvider){
 $routeProvider
   .when('/', {
   	templateUrl: 'views/home.html',
@@ -13,47 +14,12 @@ $routeProvider
   .otherwise({
     redirectTo: '/'
   });
-});
-app.controller('homeCtrl', function($scope, $log){
+}).
+controller('homeCtrl', function($log){
   $log.debug('Welcome to the testapp!');
-});
-app.controller('registerCtrl', function($scope, $log){
+}).
+controller('registerCtrl', function($scope, $log){
 	$scope.submit = function(user){
 		$log.debug('submit: user = ', user);
 	};
 });
-
-
-app.directive("passwordVerify", function() {
-   return {
-      require: "ngModel",
-      scope: {
-        passwordVerify: '='
-      },
-      link: function(scope, element, attrs, registerCtrl) {
-        scope.$watch(function() {
-            var combined;
-
-            if (scope.passwordVerify || registerCtrl.$viewValue) {
-               combined = scope.passwordVerify + '_' + registerCtrl.$viewValue; 
-            }                    
-            return combined;
-        }, function(value) {
-            if (value) {
-                registerCtrl.$parsers.unshift(function(viewValue) {
-                    var origin = scope.passwordVerify;
-                    if (origin !== viewValue) {
-                        registerCtrl.$setValidity("passwordVerify", false);
-                        return undefined;
-                    } else {
-                        registerCtrl.$setValidity("passwordVerify", true);
-                        return viewValue;
-                    }
-                });
-            }
-        });
-     }
-   };
-});
-
-
